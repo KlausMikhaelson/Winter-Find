f6a9cad35d10c33d3607cbdb2f451f305932db82
KlausMikhaelson <satyamsingh5076@gmail.com>
2022-11-19 19:57:06 +0530

    added player

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { Canvas } from '@react-three/fiber'
-import { useHelper } from "@react-three/drei";
-import { useRef } from "react";
+import { useAnimations, useHelper } from "@react-three/drei";
+import { useEffect, useRef } from "react";
 // import { OrbitControls } from '@react-three/drei/core';
 import { OrbitControls, Stats } from '@react-three/drei';
 import Lights from './components/Lightning/Lights';
@@ -9,19 +9,43 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 import Fpv from './components/Camera/Fpv';
 import { NextPage } from 'next'
 import Ground from './components/Ground/Ground'
+import {usePlane} from "@react-three/cannon"
 
 
-const GameMap = () => {
-  const model = useLoader(GLTFLoader, "./models/map.glb")
-  return (
+// const GameMap = () => {
+//   const model = useLoader(GLTFLoader, "./models/map.glb")
+//   const [ref] = usePlane(() => ({
+// 		rotation: [-Math.PI / 2, 0, 0], position: [0, -0.5, 0]
+// 	}))
+//   return (
+//     <>
+    
+//       <object3D ref={ref} position={[0, 0, 0]} scale={[20, 20, 20]}>
+//         <primitive object={model.scene} />
+//       </object3D>
+//     </>
+
+//   )
+// }
+
+const PlayerModel = () => {
+  const modelPlayer = useLoader(GLTFLoader, "./models/player.glb")
+  const {actions} = useAnimations(modelPlayer.animations, modelPlayer.scene)
+  console.log(modelPlayer)
+
+  useEffect(() => {
+    actions?.walking?.play()
+  })
+  
+  return(
     <>
-      <object3D position={[0, 0, 0]} scale={[20, 20, 20]}>
-        <primitive object={model.scene} />
-      </object3D>
+    <object3D position={[10, 3.6, 2]}>
+      <primitive object={modelPlayer.scene} />
+    </object3D>
     </>
-
   )
 }
+
 
 const Home: NextPage = () => {
 
@@ -31,8 +55,8 @@ const Home: NextPage = () => {
         <OrbitControls />
         <Lights />
         <gridHelper args={[10, 10]} />
-        <Fpv />
-        <GameMap />
+        {/* <Fpv /> */}
+        <PlayerModel />
         <Ground />
       </Canvas>
     </div>
